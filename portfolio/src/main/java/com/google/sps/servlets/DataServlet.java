3d1a805d13f6ce9097a67d2a3c9f30eeb3776eb7
Diego Velazquez Alvarez <a01027322@itesm.mx>
2020-02-29 17:59:24 -0600

    Add datastore functionality to store comments when POST.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,12 +15,16 @@
 package com.google.sps.servlets;
 
 import com.google.sps.data.Comment;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.time.LocalDate;
 import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
@@ -42,12 +46,24 @@ public class DataServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        LocalDate messageDate = LocalDate.now();
+
         String username = request.getParameter("username");
         String message = request.getParameter("message");
+        String date = messageDate.toString();
 
         Comment currentComment = new Comment(username, message);
 
         comments.add(currentComment);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        Entity commentEntity = new Entity("Comment");
+        commentEntity.setProperty("username", username);
+        commentEntity.setProperty("message", message);
+        commentEntity.setProperty("date", date);
+
+        datastore.put(commentEntity);
 
         // Redirect back to the HTML page.
         response.sendRedirect("/index.html");
