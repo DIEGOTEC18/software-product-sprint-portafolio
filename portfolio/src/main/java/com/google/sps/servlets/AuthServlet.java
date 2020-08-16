a03bd25d0a0639189b1e35d9380c53b292eb0875
DIEGOTEC18 <a01027322@itesm.mx>
2020-08-16 08:22:08 +0000

    Production bug fixes.

--- portfolio/src/main/java/com/google/sps/servlets/AuthServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AuthServlet.java
@@ -37,7 +37,11 @@ public class AuthServlet extends HttpServlet {
       String urlToRedirectToAfterUserLogsIn = "/#comment-button-div";
       String loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
 
-      response.sendRedirect(loginUrl);
+      response.getWriter().println("<head><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Comments Login</title><link rel='stylesheet' href='style.css'><link rel='shortcut icon' href='/images/favicon.ico' type='image/x-icon'><link rel='icon' href='/images/favicon.ico' type='image/x-icon'></head>");
+      response.getWriter().println("<h3 class = 'login-title lucida'>Hello stranger...</h3>");
+      response.getWriter().println("<p class = 'login-text trebuchet'>Login with Google to post a comment: </p><div class = 'center'><a href=\"" + loginUrl + "\"><img src = '/images/google_login.png' id = 'google-login-icon'</a></div>");
+
+      //response.sendRedirect(loginUrl);
 
     }
   }
